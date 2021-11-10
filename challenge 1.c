432c5a5a81097d6fab87946795b332c4d1192698
zerzkhane <zezkhanemourad@gmail.com>
2021-11-10 12:04:40 +0100

    challenge 1 c

---
+++ challenge 1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main ()
+{
+char nom[20],prenom[20];
+int age,numero;
+char sex[20];
+printf("Nom: \n");
+scanf("%s",&nom);
+printf("prenom: \n");
+scanf("%s",&prenom);
+printf("age: \n");
+scanf("%d",&age);
+printf("Sex: \n");
+scanf("%s",&sex);
+printf("Numero de telephone : \n");
+scanf("%d",&numero);
+
+printf("Nom:%s\nPrenom:%s\nAge:%d\nSexe:%s\nNumero:%d\n",nom,prenom,age,sex,numero);
+	
+	
+	
+	
+	
+}
