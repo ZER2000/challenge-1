6c5ce940c95cc32adccb9294c0c8e6e3c10d0f0f
zerzkhane <zezkhanemourad@gmail.com>
2021-11-10 14:49:45 +0100

    challenge 3

---
+++ challenge 3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+float metre,mile;
+printf("la distance en Metre:\n");
+scanf("%f",&metre);
+mile=(metre/1000)*1.609;
+printf("la distane en Mile est:%.2f",mile);
+	
+	
+}
