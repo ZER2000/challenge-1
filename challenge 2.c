94f5dda7e98f9bc53a058ec2f52910748b6063a3
zerzkhane <zezkhanemourad@gmail.com>
2021-11-10 13:55:24 +0100

    challenge 2 c

---
+++ challenge 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+float C,F;
+printf("la température en degre Celsius: \n");
+scanf("%f",&F);
+C=(C*1.8)+32;
+
+printf("La température en Fahrenheit est:%.2f",C);
+	
+	
+}
