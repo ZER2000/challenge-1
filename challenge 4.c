48142a7061705af0f76e358b8fc186ae6802b956
zerzkhane <zezkhanemourad@gmail.com>
2021-11-10 15:19:07 +0100

    challenge4

---
+++ challenge 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+float mile,metre;
+printf("la distance en Mile:\n");
+scanf("%f",&mile);
+float klm=mile/1.609;
+metre=klm*1000;
+printf( "la distance en Metre:\n%.2f",metre);
+	
+	
+}
