e71dd775d12b4868827b815903956ec2f883ae71
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 16:47:56 +0100

    challenge c

---
+++ challenge condition (5).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+	int a,b,somme,c;
+printf("donner la valeur de A:\n");
+scanf("%d",&a);
+printf("donner la valeur de B \n");
+scanf("%d",&b);
+ somme=a+b;
+  if(a==b){
+c=somme*3;
+printf("la valeur est :%d\n",c);
+ }	
+else printf ("la valeur est :%d\n",somme);
+
+}
