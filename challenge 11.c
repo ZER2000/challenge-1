cfab8e455e100f991af6331b666d54d0bc904f90
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 11:25:35 +0100

    challenge c

---
+++ challenge 11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+  void main()
+{
+float lon,lar,C;
+printf ("donner la valeur de longueur : \n");
+scanf ("%f",&lon);
+printf ("donner la valeur de la largeur : \n");
+scanf ("%f",&lar);
+C=(lon+lar)*2;
+
+printf ("la circonférence d'un rectangle est :\n%.2f",C);
+
+
+
+	
+	
+}
