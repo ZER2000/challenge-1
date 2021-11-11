308fefd5e562f4db2e6db2757382bdf12d90bdd3
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 16:07:36 +0100

    challenge c

--- challenge condition (1).c
+++ challenge condition (1).c
@@ -1,26 +1,10 @@
-
 #include<stdlib.h>
-void main()
+#include<stdio.h>
+void num()
 {
-float F,C;
-printf("La temperature en Fahrenheit:\n");	
-scanf("%f",&F);
-C=(F-32)/1.8;
-printf("La temperature en degre Celsius:\n%.2f\n",C);
-  
-   if (C<0){
-printf("la sensation ressentie  tres Froid \n");	
-}
-  else if (C<10){
+int A,b;
 	
-	printf("la sensation ressentie Froid \n");
-}
-else if (C>=40){
-	printf("la sensation ressentie  tres Chaud \n");
-}
-
-else if (C>=20){
-	printf("la sensation ressentie Chaud \n");
-}
-
+	
+	
+	
 }
---
+++ challenge condition (3).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main ()
+{
+	
+	int a,b;
+printf ("donner la valeur de A:\n");
+scanf("%d",&a);
+printf ("donner la valeur de B:\n");
+scanf("%d",&b);
+ if (a<b){
+printf("A inferieure de B \n");
+ }
+ else if (a>b){
+printf("A superieure de B\n");
+ }
+  else if (a==b){
+printf("A egale B\n");
+  }
+	
+	
+}
+
