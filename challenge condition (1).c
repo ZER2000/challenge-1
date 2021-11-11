f00d57b04021ae630672437d00d03c3428845382
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 15:15:36 +0100

    challeng c

---
+++ challenge condition (1).c
@@ -0,0 +1,26 @@
+
+#include<stdlib.h>
+void main()
+{
+float F,C;
+printf("La temperature en Fahrenheit:\n");	
+scanf("%f",&F);
+C=(F-32)/1.8;
+printf("La temperature en degre Celsius:\n%.2f\n",C);
+  
+   if (C<0){
+printf("la sensation ressentie  tres Froid \n");	
+}
+  else if (C<10){
+	
+	printf("la sensation ressentie Froid \n");
+}
+else if (C>=40){
+	printf("la sensation ressentie  tres Chaud \n");
+}
+
+else if (C>=20){
+	printf("la sensation ressentie Chaud \n");
+}
+
+}
