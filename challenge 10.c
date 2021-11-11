9acd95132977f480cc7d5d678395deadf248e99e
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 11:04:36 +0100

    challenge c

---
+++ challenge 10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+float R,C;
+float P=3.14;
+printf("donner la valeur de rayon du cercle : \n");
+scanf("%f",&R);
+C=2*P*R;
+
+printf("la circonference de cercle est : \n%.2f",C);
+
+}
+
