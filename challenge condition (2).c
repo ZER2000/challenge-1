ad82db093acd3441819fb103d1ef41afb51a3541
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 15:32:33 +0100

    challenge c

---
+++ challenge condition (2).c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+void main()
+{
+int nombre;
+printf("donner votre nombre: \n");
+scanf("%d",&nombre);
+int div=nombre%2;
+
+if (div==0){
+printf("le nombre est pair \n");
+}
+	else printf("le nombre est impair \n");
+	
+	
+	
+	
+}
