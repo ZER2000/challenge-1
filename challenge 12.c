c2e65de823acc1232371f8585be3f240a83a2af3
zerzkhane <zezkhanemourad@gmail.com>
2021-11-11 13:40:44 +0100

    challenge c

---
+++ challenge 12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main ()
+{
+int nombre,a,b,c;
+printf("donner votre nombre : \n");
+scanf("%d",&nombre);
+a=((nombre%100)%10);
+b=((nombre%100)/10);
+c=nombre/100;
+
+printf("l'inverse de nombre est: \n %d%d%d%",a,b,c);
+	
+	
+	
+}
